049a05b975cc70c212137459a9d0800a728a1161
jvargas <jvargas@ximdex.com>
2017-02-08 16:14:07 +0100

    Added new Extensions.php

---
+++ lib/Ximdex/Utils/Extensions.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace Ximdex\Utils;
+use App;
+
+class Extensions {
+    const JQUERY_PATH = "/extensions/jquery";
+    const JQUERY = "/extensions/jquery/jquery-1.8.3.min.js";
+    const JQUERY_UI = "/extensions/jquery/jquery-ui-1.9.1.custom.min.js";
+    const SMARTY = "/extensions/smarty/libs/Smarty.class.php";
+    const PHPSECLIB = "/extensions/phpseclib";
+    const BOOTSTRAP = "/extensions/bootstrap/dist";
+
+    /**
+     * if function is called then we retrieve  UrlRoot + constant
+     * @param String $_func attribute name. e.g: Ximdex\Utils\Extensions::smarty() -> smarty
+     * @param Array $_args ignored
+     * @return UrlRoot+attribute value
+     */
+    public static function __callStatic($_func, $_args) {
+        $const = strtoupper( $_func );
+        $urlRoot = App::getValue( 'UrlRoot' );
+        $value = constant( sprintf( '%s::%s', "self", $const ) );
+        return $urlRoot . $value;
+    }
+
+}
