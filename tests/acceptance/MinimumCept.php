557dab9ddac2abfd0c0f8be236e7d4a0701d37b4
Jesús Manuel Vargas <estudiantejm@gmail.com>
2017-02-09 22:15:40 +0100

    Using builded images

--- tests/acceptance/MinimumCept.php
+++ tests/acceptance/MinimumCept.php
@@ -24,19 +24,15 @@ $I->fillField("dbname", "ximdex");
 
 $I->click("Check");
 
-$I->wait(2);
+$I->wait(3);
 
 $I->click("Continue: Create tables");
 
-$I->wait(10);
-
-$I->see("Tables and default data created");
+$I->waitForText("Tables and default data created", 15);
 
 $I->click("Continue: Modules");
 
-$I->wait(5);
-
-$I->see("Install Modules");
+$I->waitForText("Install Modules", 5);
 
 $I->click("Next: Settings");
 
@@ -57,46 +53,38 @@ $I->fillField("password", "ximdex");
 
 $I->click("Sign in");
 
-$I->wait(5);
-
-$I->see("Welcome to Ximdex CMS, ximdex!");
+$I->waitForText("Welcome to Ximdex CMS, ximdex!", 5);
 
 $I->wantTo('ensure that publish works');
 
-$I->wait(2);
-
-
-$I->see("Hello ximdex, first time here?", "#tourcontrols");
+$I->waitForText("Hello ximdex, first time here?", 5, "#tourcontrols");
 $I->click("#canceltour");
-
 
 $I->doubleClick("//span[contains(text(),'Picasso')]", "#angular-tree");
 
-$I->see("Picasso_Server", "#angular-tree");
+$I->waitForText("Picasso_Server", 5, "#angular-tree");
 
 $I->doubleClick("//span[contains(text(),'Picasso_Server')]", "#angular-tree");
-$I->see("documents", "#angular-tree");
+$I->waitForText("documents", 5, "#angular-tree");
 
 $I->doubleClick("//span[contains(text(),'documents')]", "#angular-tree");
-$I->see("picasso", "#angular-tree");
+$I->waitForText("picasso", 5, "#angular-tree");
 
 $I->doubleClick("//span[contains(text(),'picasso')]", "#angular-tree");
-$I->see("picasso-iden", "#angular-tree");
+$I->waitForText("picasso-iden", 5, "#angular-tree");
 
 // Open picasso-iden menu
 $I->click("//*[@id=\"angular-tree\"]/div[1]/div[2]/div/div[1]/div[2]/xim-tree/div/div[2]/ul/li/tree-node/span/ul/li/span/ul/li[4]/span/ul/li[3]/span/ul/li[1]/span/ul/li/span/div/span[2]");
 
-$I->see("Publish", "body > div.xim-actions-menu.destroy-on-click.noselect.xim-actions-menu-list");
+$I->waitForText("Publish", 3, "body > div.xim-actions-menu.destroy-on-click.noselect.xim-actions-menu-list");
 
 $I->click("body > div.xim-actions-menu.destroy-on-click.noselect.xim-actions-menu-list > div.button-container-list.icon.workflow_forward");
 
-$I->wait(1);
+$I->wait(3);
 
 $I->click("Publish", "#angular-content");
 
-$I->wait(3);
-
-$I->see("State has been successfully changed", "#angular-content");
+$I->waitForText("State has been successfully changed", 3, "#angular-content");
 
 $I->wait(60);
 
@@ -107,4 +95,3 @@ $I->amOnPage("/data/previos/picasso-iden-idhtml.html");
 
 $I->see("Picasso", ".header");
 $I->see("Cubism", ".header");
-
