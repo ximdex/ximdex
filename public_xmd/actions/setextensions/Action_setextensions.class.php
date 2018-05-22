db26b9a0e43d81bf66cfe5e55e21d62c48f21774
Jesus Dorado Rodriguez <jesusdrjesus@gmail.com>
2018-05-22 14:42:07 +0200

    Including missing headers and modification ui

--- public_xmd/actions/setextensions/Action_setextensions.class.php
+++ public_xmd/actions/setextensions/Action_setextensions.class.php
@@ -4,7 +4,6 @@ use Ximdex\Models\NodeType;
 use Ximdex\MVC\ActionAbstract;
 use Ximdex\Models\Node;
 
-
 class Action_setextensions extends ActionAbstract
 {
     public function index()
@@ -16,7 +15,9 @@ class Action_setextensions extends ActionAbstract
 
         $this->addCss('/actions/setextensions/resources/css/style.css');
 
-        $values = array('commonAllowedExtensions' => json_encode($commonAllowedExtensions));
+        $values = array('commonAllowedExtensions' => json_encode($commonAllowedExtensions),
+        'node_Type' => $node->nodeType->GetName(),
+        );
 
         $this->render($values, null, 'default-3.0.tpl');
     }
@@ -29,14 +30,14 @@ class Action_setextensions extends ActionAbstract
         $patt = '/^((\w+|\*))?(,(\w+|\*))*\s*$/i';
         $res = true;
 
-        foreach($extensions as $id => $ext){
+        foreach ($extensions as $id => $ext) {
             $res &= preg_match($patt, trim($ext["extension"]));
             $extensions[$id]["extension"] = preg_split("/,/", trim($ext["extension"]), 0, PREG_SPLIT_NO_EMPTY);
-            if(in_array("*", $extensions[$id]["extension"])){
+            if (in_array("*", $extensions[$id]["extension"])) {
                 $extensions[$id]["extension"] = array("*");
             }
         }
-        if(!$res){
+        if (!$res) {
             $values = array(
                 'result' => "fail",
                 'message' => _("There are errors on extensions")
@@ -44,16 +45,16 @@ class Action_setextensions extends ActionAbstract
             $this->sendJSON($values);
         }
 
-        foreach($extensions as $id => $ext){
-            foreach($extensions as $id2 => $ext2){
-                if($id < $id2){
+        foreach ($extensions as $id => $ext) {
+            foreach ($extensions as $id2 => $ext2) {
+                if ($id < $id2) {
                     $count = count(array_intersect($ext["extension"], $ext2["extension"]));
                     $res &= $count == 0;
                 }
             }
         }
 
-        if(!$res){
+        if (!$res) {
             $values = array(
                 'result' => "fail",
                 'message' => _("Extensions have ambiguous values")
@@ -61,14 +62,14 @@ class Action_setextensions extends ActionAbstract
             $this->sendJSON($values);
         }
 
-        foreach($extensions as $ext){
+        foreach ($extensions as $ext) {
             $e = new \Ximdex\Models\RelNodeTypeMimeType($ext["id"]);
-            if(count($ext["extension"])==0){
-                $e->set("extension","");
-            }elseif(isset($ext["extension"][0]) && $ext["extension"][0]=="*"){
-                $e->set("extension","*");
-            }else{
-                $e->set("extension",";".implode(";",$ext["extension"]).";");
+            if (count($ext["extension"])==0) {
+                $e->set("extension", "");
+            } elseif (isset($ext["extension"][0]) && $ext["extension"][0]=="*") {
+                $e->set("extension", "*");
+            } else {
+                $e->set("extension", ";".implode(";", $ext["extension"]).";");
             }
             $e->update();
         }
@@ -81,5 +82,4 @@ class Action_setextensions extends ActionAbstract
 
         $this->sendJSON($values);
     }
-
 }
